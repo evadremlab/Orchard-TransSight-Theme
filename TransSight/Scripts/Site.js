4c9b9affc297d1afe10feccd5ff0a285a2fd3f9d
evadremlab <david.balmer@transsight.com>
2017-01-12 16:36:30 -0800

    Inital commit

---
+++ TransSight/Scripts/Site.js
@@ -0,0 +1,11 @@
+﻿$(function () {
+    // Add pagename class to the body tag
+    var parts = window.location.pathname.split('/');
+    var pageName = parts[parts.length - 1];
+    $('body').addClass('body-' + (pageName ? pageName.toLowerCase() : 'home'));
+
+    // Adjust footer position if shape tracing toolbar is present
+    if ($('#shape-tracing-container').length) {
+        $('#layout-footer').css('bottom', '2rem');
+    }
+});
